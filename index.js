fbf1cf02193d7fa7bc99efe2f9694452eab8211b
DanyCarter <danielcadenas88@gmail.com>
2023-10-03 10:50:58 +0200

    Actividad No Evaluable

---
+++ index.js
@@ -0,0 +1,18 @@
+// Daniel Cadenas Dominguez
+// Repositorio GitHub:
+
+const numero = prompt("Por favor ingrese numero");
+
+if (numero > 0) {
+    console.log("El numero ingresado es positivo.");
+    alert("El numero ingresado es positivo.");
+
+} else if (numero < 0) {
+    console.log("El numero ingresado es negativo.");
+    alert("El numero ingresado es negativo.");
+
+} else {
+    console.log("El numero ingresado es igual a cero.");
+    alert("El numero ingresado es igual a cero.");
+}
+
